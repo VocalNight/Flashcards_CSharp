92887271f21185d5198b72335ee9945a65e5cd6a
Vocal <vocalnight@protonmail.com>
2023-05-07 01:12:24 -0300

    Add project files.

---
+++ Model/Cards.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Model {
+    internal class Cards {
+
+        public String Front { get; set; }
+        public String Back { get; set; }
+
+        public String DeckId { get; set; }
+
+    }
+}
